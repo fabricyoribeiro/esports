df963d24582a33e253828013cb38d397e10a2af7
Fabricyo Ribeiro <69694301+fabricyoribeiro@users.noreply.github.com>
2022-10-04 23:34:58 -0300

    iniciei mobile, web e server

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,8 @@ app.get('/ads', (request, response) => {
   return response.json([
     {id: 1, name: 'Anuncio 1'},
     {id: 2, name: 'Anuncio 2'},
-    {id: 3, name: 'Anuncio 3'}
+    {id: 3, name: 'Anuncio 3'},
+    {id: 4, name: 'Anuncio 4'}
   ])
 })
 
---
+++ web/src/App.tsx
@@ -0,0 +1,7 @@
+
+
+function App() {
+  return <h1>Helo nlw</h1>
+}
+
+export default App
